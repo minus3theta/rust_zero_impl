d1ddce197d975578699ea904c04e82a5a9354eea
minus3theta <yone_j_synthesis@yahoo.co.jp>
2023-02-02 22:33:07 +0900

    [shell] Implement execution

---
+++ zerosh/src/main.rs
@@ -0,0 +1,17 @@
+mod shell;
+
+const HISTORY_FILE: &str = ".zerosh_history";
+
+fn main() -> anyhow::Result<()> {
+    let mut logfile = HISTORY_FILE;
+    let mut home = dirs::home_dir();
+    if let Some(h) = &mut home {
+        h.push(HISTORY_FILE);
+        logfile = h.to_str().unwrap_or(HISTORY_FILE);
+    }
+
+    let sh = shell::Shell::new(logfile);
+    sh.run()?;
+
+    Ok(())
+}
---
+++ zerosh/src/shell.rs
@@ -0,0 +1,452 @@
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    ffi::CString,
+    process::exit,
+    sync::mpsc::{channel, sync_channel, Receiver, Sender, SyncSender},
+    thread,
+};
+
+use anyhow::bail;
+use nix::{
+    libc,
+    sys::{
+        signal::{signal, SigHandler, Signal},
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    unistd::{self, dup2, execvp, fork, setpgid, tcgetpgrp, tcsetpgrp, ForkResult, Pid},
+};
+use rustyline::{error::ReadlineError, Editor};
+use signal_hook::{consts::*, iterator::Signals};
+
+fn syscall<F, T>(f: F) -> Result<T, nix::Error>
+where
+    F: Fn() -> Result<T, nix::Error>,
+{
+    loop {
+        match f() {
+            Err(nix::Error::EINTR) => (),
+            result => return result,
+        }
+    }
+}
+
+enum WorkerMsg {
+    Signal(i32),
+    Cmd(String),
+}
+
+enum ShellMsg {
+    Continue(i32),
+    Quit(i32),
+}
+
+#[derive(Debug)]
+pub struct Shell {
+    logfile: String,
+}
+
+impl Shell {
+    pub fn new(logfile: &str) -> Self {
+        Self {
+            logfile: logfile.to_string(),
+        }
+    }
+
+    /// mainスレッド
+    pub fn run(&self) -> Result<(), anyhow::Error> {
+        unsafe { signal(Signal::SIGTTOU, SigHandler::SigIgn).unwrap() };
+
+        let mut rl = Editor::<()>::new()?;
+        if let Err(e) = rl.load_history(&self.logfile) {
+            eprintln!("ZeroSh: ヒストリファイルの読み込みに失敗: {e}");
+        }
+
+        let (worker_tx, worker_rx) = channel();
+        let (shell_tx, shell_rx) = sync_channel(0);
+        spawn_sig_handler(worker_tx.clone())?;
+        Worker::new().spawn(worker_rx, shell_tx);
+
+        let exit_val;
+        let mut prev = 0;
+        loop {
+            let face = if prev == 0 { '\u{1F642}' } else { '\u{1F480}' };
+            match rl.readline(&format!("ZeroSh {face} %> ")) {
+                Ok(line) => {
+                    let line_trimed = line.trim();
+                    if line_trimed.is_empty() {
+                        continue;
+                    } else {
+                        rl.add_history_entry(line_trimed);
+                    }
+
+                    worker_tx.send(WorkerMsg::Cmd(line)).unwrap();
+                    match shell_rx.recv().unwrap() {
+                        ShellMsg::Continue(n) => prev = n,
+                        ShellMsg::Quit(n) => {
+                            exit_val = n;
+                            break;
+                        }
+                    }
+                }
+                Err(ReadlineError::Interrupted) => eprintln!("ZeroSh: 終了はCtrl+d"),
+                Err(ReadlineError::Eof) => {
+                    worker_tx.send(WorkerMsg::Cmd("exit".to_string())).unwrap();
+                    match shell_rx.recv().unwrap() {
+                        ShellMsg::Quit(n) => {
+                            exit_val = n;
+                            break;
+                        }
+                        _ => panic!("exitに失敗"),
+                    }
+                }
+                Err(e) => {
+                    eprintln!("ZeroSh: 読み込みエラー\n{e}");
+                    exit_val = 1;
+                    break;
+                }
+            }
+        }
+
+        if let Err(e) = rl.save_history(&self.logfile) {
+            eprintln!("ZeroSh: ヒストリファイルへの書き込みに失敗: {e}");
+        }
+        exit(exit_val);
+    }
+}
+
+fn spawn_sig_handler(tx: Sender<WorkerMsg>) -> Result<(), anyhow::Error> {
+    let mut signals = Signals::new(&[SIGINT, SIGTSTP, SIGCHLD])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            tx.send(WorkerMsg::Signal(sig)).unwrap();
+        }
+    });
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum ProcState {
+    Run,
+    Stop,
+}
+
+#[derive(Debug, Clone)]
+struct ProcInfo {
+    state: ProcState,
+    pgid: Pid,
+}
+
+#[derive(Debug)]
+struct Worker {
+    exit_val: i32,
+    fg: Option<Pid>,
+
+    jobs: BTreeMap<usize, (Pid, String)>,
+
+    pgid_to_pids: HashMap<Pid, (usize, HashSet<Pid>)>,
+
+    pid_to_info: HashMap<Pid, ProcInfo>,
+    shell_pgid: Pid,
+}
+
+impl Worker {
+    fn new() -> Self {
+        Self {
+            exit_val: 0,
+            fg: None,
+            jobs: Default::default(),
+            pgid_to_pids: Default::default(),
+            pid_to_info: Default::default(),
+            shell_pgid: tcgetpgrp(libc::STDIN_FILENO).unwrap(),
+        }
+    }
+
+    fn spawn(mut self, worker_rx: Receiver<WorkerMsg>, shell_tx: SyncSender<ShellMsg>) {
+        thread::spawn(move || {
+            for msg in worker_rx.iter() {
+                match msg {
+                    WorkerMsg::Cmd(line) => match parse_cmd(&line) {
+                        Ok(cmd) => {
+                            if self.built_in_cmd(&cmd, &shell_tx) {
+                                continue;
+                            }
+
+                            if !self.spawn_child(&line, &cmd) {
+                                shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("ZeroSh: {e}");
+                            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+                        }
+                    },
+                    WorkerMsg::Signal(SIGCHLD) => {
+                        self.wait_child(&shell_tx);
+                    }
+                    _ => (),
+                }
+            }
+        });
+    }
+
+    fn built_in_cmd(&mut self, cmd: &[(&str, Vec<&str>)], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        if cmd.len() > 1 {
+            return false;
+        }
+
+        match cmd[0].0 {
+            "exit" => self.run_exit(&cmd[0].1, shell_tx),
+            "jobs" => self.run_jobs(shell_tx),
+            "fg" => self.run_fg(&cmd[0].1, shell_tx),
+            "cd" => self.run_cd(&cmd[0].1, shell_tx),
+            _ => false,
+        }
+    }
+
+    fn run_exit(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        if !self.jobs.is_empty() {
+            eprintln!("ジョブが実行中なので終了できません");
+            self.exit_val = 1;
+            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+            return true;
+        }
+
+        let exit_val = if let Some(s) = args.get(1) {
+            if let Ok(n) = (*s).parse::<i32>() {
+                n
+            } else {
+                eprintln!("{s}は不正な引数です");
+                self.exit_val = 1;
+                shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+                return true;
+            }
+        } else {
+            self.exit_val
+        };
+
+        shell_tx.send(ShellMsg::Quit(exit_val)).unwrap();
+        true
+    }
+
+    fn run_jobs(&self, shell_tx: &SyncSender<ShellMsg>) -> bool {
+        todo!()
+    }
+
+    fn run_fg(&self, cmd: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        todo!()
+    }
+
+    fn run_cd(&self, cmd: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        todo!()
+    }
+
+    fn spawn_child(&mut self, line: &str, cmd: &[(&str, Vec<&str>)]) -> bool {
+        assert_ne!(cmd.len(), 0);
+
+        let job_id = if let Some(id) = self.get_new_job_id() {
+            id
+        } else {
+            eprintln!("ZeroSh: 管理可能なジョブの最大値に到達");
+            return false;
+        };
+
+        if cmd.len() >= 2 {
+            todo!("pipe is not supported yet");
+        }
+
+        let mut output = None;
+
+        let pgid;
+        match fork_exec(Pid::from_raw(0), cmd[0].0, &cmd[0].1, None, output) {
+            Ok(child) => {
+                pgid = child;
+            }
+            Err(e) => {
+                eprintln!("ZeroSh: プロセス生成エラー: {e}");
+                return false;
+            }
+        }
+
+        let info = ProcInfo {
+            state: ProcState::Run,
+            pgid,
+        };
+        let mut pids = HashMap::new();
+        pids.insert(pgid, info.clone());
+
+        self.fg = Some(pgid);
+        self.insert_job(job_id, pgid, pids, line);
+        tcsetpgrp(libc::STDIN_FILENO, pgid).unwrap();
+
+        true
+    }
+
+    fn wait_child(&mut self, shell_tx: &SyncSender<ShellMsg>) {
+        let flag = Some(WaitPidFlag::WUNTRACED | WaitPidFlag::WNOHANG | WaitPidFlag::WCONTINUED);
+
+        loop {
+            match syscall(|| waitpid(Pid::from_raw(-1), flag)) {
+                Ok(WaitStatus::Exited(pid, status)) => {
+                    self.exit_val = status;
+                    self.process_term(pid, shell_tx);
+                }
+                Ok(WaitStatus::Signaled(pid, signal, core)) => {
+                    todo!()
+                }
+                Ok(WaitStatus::Stopped(pid, _sig)) => self.process_stop(pid, shell_tx),
+                Ok(WaitStatus::Continued(pid)) => todo!(),
+                Ok(WaitStatus::StillAlive) => return,
+                Err(nix::Error::ECHILD) => return,
+                Err(e) => {
+                    eprintln!("\nZeroSh: waitが失敗: {e}");
+                    exit(1);
+                }
+
+                #[cfg(any(target_os = "linux", target_os = "android"))]
+                Ok(WaitStatus::PtraceEvent(pid, _, _) | WaitStatus::PtraceSyscall(pid)) => {
+                    self.process_stop(pid, shell_tx);
+                }
+            }
+        }
+    }
+
+    fn process_term(&mut self, pid: Pid, shell_tx: &SyncSender<ShellMsg>) {
+        if let Some((job_id, pgid)) = self.remove_pid(pid) {
+            self.manage_job(job_id, pgid, shell_tx);
+        }
+    }
+
+    fn process_stop(&self, pid: Pid, shell_tx: &SyncSender<ShellMsg>) {
+        todo!()
+    }
+
+    fn manage_job(&mut self, job_id: usize, pgid: Pid, shell_tx: &SyncSender<ShellMsg>) {
+        let is_fg = self.fg.map_or(false, |x| pgid == x);
+        let line = &self.jobs.get(&job_id).unwrap().1;
+        if is_fg {
+            if self.is_group_empty(pgid) {
+                eprintln!("[{job_id}] 終了\t{line}");
+                self.remove_job(job_id);
+                self.set_shell_fg(shell_tx);
+            } else if self.is_group_stop(pgid).unwrap() {
+                todo!()
+            }
+        } else if self.is_group_empty(pgid) {
+            eprintln!("\n[{job_id}] 終了\t{line}");
+            self.remove_job(job_id);
+        }
+    }
+
+    fn insert_job(&mut self, job_id: usize, pgid: Pid, pids: HashMap<Pid, ProcInfo>, line: &str) {
+        assert!(!self.jobs.contains_key(&job_id));
+        self.jobs.insert(job_id, (pgid, line.to_owned()));
+
+        let mut procs = HashSet::new();
+        for (pid, info) in pids {
+            procs.insert(pid);
+
+            assert!(!self.pid_to_info.contains_key(&pid));
+            self.pid_to_info.insert(pid, info);
+        }
+
+        assert!(!self.pgid_to_pids.contains_key(&pgid));
+        self.pgid_to_pids.insert(pgid, (job_id, procs));
+    }
+
+    fn remove_job(&mut self, job_id: usize) {
+        if let Some((pgid, _)) = self.jobs.remove(&job_id) {
+            if let Some((_, pids)) = self.pgid_to_pids.remove(&pgid) {
+                assert!(pids.is_empty());
+            }
+        }
+    }
+
+    fn is_group_empty(&self, pgid: Pid) -> bool {
+        self.pgid_to_pids.get(&pgid).unwrap().1.is_empty()
+    }
+
+    fn is_group_stop(&self, pgid: Pid) -> Option<bool> {
+        todo!()
+    }
+
+    fn remove_pid(&mut self, pid: Pid) -> Option<(usize, Pid)> {
+        let pgid = self.pid_to_info.get(&pid)?.pgid;
+        let it = self.pgid_to_pids.get_mut(&pgid)?;
+        it.1.remove(&pid);
+        let job_id = it.0;
+        Some((job_id, pgid))
+    }
+
+    fn set_shell_fg(&mut self, shell_tx: &SyncSender<ShellMsg>) {
+        self.fg = None;
+        tcsetpgrp(libc::STDIN_FILENO, self.shell_pgid).unwrap();
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap();
+    }
+
+    fn get_new_job_id(&self) -> Option<usize> {
+        for i in 0..=usize::MAX {
+            if !self.jobs.contains_key(&i) {
+                return Some(i);
+            }
+        }
+        None
+    }
+}
+
+fn fork_exec(
+    pgid: Pid,
+    filename: &str,
+    args: &[&str],
+    input: Option<i32>,
+    output: Option<i32>,
+) -> Result<Pid, anyhow::Error> {
+    let filename = CString::new(filename)?;
+    let args: Vec<CString> = args.iter().map(|s| CString::new(*s).unwrap()).collect();
+
+    match syscall(|| unsafe { fork() })? {
+        ForkResult::Parent { child, .. } => {
+            setpgid(child, pgid).unwrap();
+            Ok(child)
+        }
+        ForkResult::Child => {
+            setpgid(Pid::from_raw(0), pgid).unwrap();
+
+            if let Some(infd) = input {
+                syscall(|| dup2(infd, libc::STDIN_FILENO)).unwrap();
+            }
+            if let Some(outfd) = output {
+                syscall(|| dup2(outfd, libc::STDOUT_FILENO)).unwrap();
+            }
+
+            for i in 3..=6 {
+                let _ = syscall(|| unistd::close(i));
+            }
+
+            match execvp(&filename, &args) {
+                Err(_) => {
+                    unistd::write(libc::STDERR_FILENO, "不明なコマンドを実行\n".as_bytes()).ok();
+                    exit(1);
+                }
+                Ok(_) => unreachable!(),
+            }
+        }
+    }
+}
+
+type CmdResult<'a> = Result<Vec<(&'a str, Vec<&'a str>)>, anyhow::Error>;
+
+fn parse_cmd(line: &str) -> CmdResult {
+    let commands = line.split(" | ");
+    commands
+        .map(|cmd| {
+            let cmd = cmd.trim();
+            if cmd.is_empty() {
+                bail!("ParseError: empty command");
+            }
+            let mut words = cmd.split(' ');
+            let name = words.next().unwrap();
+            Ok((name, words.collect()))
+        })
+        .collect()
+}
